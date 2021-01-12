d1e7fa01bee2283346751ee0fed7fcaa7433a5a9
Vardan135 <67186965+Vardan135@users.noreply.github.com>
2021-01-12 17:07:40 +0400

    Update auth.js

--- components/scripts/auth.js
+++ components/scripts/auth.js
@@ -5,6 +5,63 @@ const pass_forgot = document.querySelector("#pass_forgot");
 const reset_submit = document.querySelector("#submit_reset");
 const back_btn = document.querySelector("#back_btn");
 
+log_in.addEventListener("click", button => {
+    button.preventDefault();
+    if (validEmail(document.forms.form_log_in.email.value) && document.forms.form_log_in.pass.value) {
+        let data = {
+            email: document.forms.form_log_in.email.value,
+            password: document.forms.form_log_in.pass.value
+        }
+        //մուտքի տվյալներ JSON.stringify(data)
+        console.log(JSON.stringify(data));
+    }
+    else {
+        if (!document.forms.form_log_in.email.value) {
+            document.forms.form_log_in.email.classList.add("red");
+        } else {
+            document.forms.form_log_in.email.classList.remove("red");
+        }
+        if (!document.forms.form_log_in.pass.value) {
+            document.forms.form_log_in.pass.classList.add("red");
+        } else {
+            document.forms.form_log_in.pass.classList.remove("red");
+        }
+        console.log("You are not authorized");
+    }
+});
+
+sign_up.addEventListener("click", button => {
+    button.preventDefault();
+    if (validEmail(document.forms.form_sign_up.email.value) && document.forms.form_sign_up.name.value && document.forms.form_sign_up.pass && document.forms.form_sign_up.terms.checked) {
+
+        let create_data = {
+            name: document.forms.form_sign_up.name.value,
+            email: document.forms.form_sign_up.email.value,
+            password: document.forms.form_sign_up.pass.value
+        }
+        // ստեղծման տվյանլեր JSON.stringify(create_data)
+        console.log(JSON.stringify(create_data));
+    }
+    else {
+        if (!document.forms.form_sign_up.name.value) {
+            document.forms.form_sign_up.name.classList.add("red");
+        } else {
+            document.forms.form_sign_up.name.classList.remove("red");
+        }
+        if (!document.forms.form_sign_up.email.value) {
+            document.forms.form_sign_up.email.classList.add("red");
+        } else {
+            document.forms.form_sign_up.email.classList.remove("red");
+        }
+        if (!document.forms.form_sign_up.pass.value) {
+            document.forms.form_sign_up.pass.classList.add("red");
+        } else {
+            document.forms.form_sign_up.pass.classList.remove("red");
+        }
+        console.log("no to comp");
+    }
+});
+
 nav_item.forEach(btn => {
     btn.addEventListener("click", e => {
         nav_item.forEach(e => e.style.borderBottom = "none");
@@ -32,14 +89,6 @@ pass_forgot.addEventListener("click", () => {
 
 });
 
-log_in.addEventListener("click", button => {
-    button.preventDefault();
-});
-
-sign_up.addEventListener("click", button => {
-    button.preventDefault();
-});
-
 reset_submit.addEventListener("click", button => {
     button.preventDefault();
     if (document.forms.form_reset.email_reset.value) {
@@ -50,7 +99,8 @@ reset_submit.addEventListener("click", button => {
         <div style="text-align:center;">
         <img src="../images/icon.png" id="done_img">
         <h2 style="text-align:center;">Success !</h2>
-        <p>A email has been send to <span style="font-weight: bold;">${document.forms.form_reset.email_reset.value}</span>. Please check for an email from company and click on the included link to reset your password.</p>
+        <p>A email has been send to <span style="font-weight: bold;">${document.forms.form_reset.email_reset.value}</span>.
+        Please check for an email from company and click on the included link to reset your password.</p>
         <button id="back_home_btn" class="submit">Back to Home</button>
         <div>
         `;
@@ -73,14 +123,7 @@ back_btn.addEventListener("click", () => {
     document.querySelector("#auth_form_cont").style.display = "block";
     document.querySelector("#auth_form").classList.remove("pass_flex");
 });
-
-/*
------------------------------------------------------
-Մեյլը ստուգելու համար
------------------------------------------------------
-function validateEmail(email)
-    {
-        var re = /\S+@\S+\.\S+/;
-        return re.test(email);
-    }
-*/
+let validEmail = function (email) {
+    let re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
