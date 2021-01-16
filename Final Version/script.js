03bccad3dcccb4d1f7c1439828bcd3396b90fa47
Vardan135 <67186965+Vardan135@users.noreply.github.com>
2021-01-16 15:00:24 +0400

    Update script.js

--- Final Version/script.js
+++ Final Version/script.js
@@ -1,89 +1,122 @@
-
-
-// The Left Menu
-
-//icons
-const leftMenuIcons = document.querySelectorAll('#left-menu-selector>ul>li')
-let activeLeftMenuIcon = document.querySelector('#left-menu-selector>ul>li.active')
-//menus
-const leftMenus = document.querySelectorAll('#the-menus>ul')
-let activeLeftMenu = document.querySelector('#the-menus>ul.active')
-
-
-
+const leftMenuIcons = document.querySelectorAll('.left_menu_selector_item');
+const menu_close_icon = document.querySelector("#menu_close_icon");
+const menuOpen = document.querySelector('.menu_open');
+let activeLeftMenu = document.querySelectorAll('.menu');
+let leftMenuDrops = document.querySelectorAll('.drop_active');
 
 // opening the menus
-leftMenuIcons.forEach((element=>{
-    element.addEventListener('click', (e)=>{
 
-        if(element != activeLeftMenuIcon){  // check if the selected menu is already active or not
-            activeLeftMenuIcon.classList.remove('active')
-            element.classList.add('active')
-            activeLeftMenuIcon = document.querySelector('#left-menu-selector>ul>li.active')
-            //change the active icon
-
-            activeLeftMenu.classList.remove('active')
-
-            leftMenus.forEach(menu=>{
-                if(menu.id == `${element.id}-menu`){
-                    menu.classList.add('active')
-                    activeLeftMenu = document.querySelector('#the-menus>ul.active')
-
-                    
-                  
-                }
-            })
-
-            // change the active menu
-            
-
-
+leftMenuIcons.forEach((element => {
+    element.addEventListener('click', () => {
+        if (!element.classList.contains("active")) {
+            leftMenuIcons.forEach(e => e.classList.remove("active"));
+            element.classList.add("active");
+            menuOpen.classList.add('show');
+        } else {
+            element.classList.remove("active");
+            menuOpen.classList.remove('show');
         }
 
+        activeLeftMenu.forEach(e => e.classList.remove("show"));
+        let activeElement = `${element.id}_menu`;
+        document.querySelector(`#${activeElement}`).classList.add('show');
 
+    });
+}));
 
-    })
-}))
+// menu close button
 
-
-
-
+menu_close_icon.addEventListener("click", () => {
+    if (!menuOpen.classList.contains("show")) {
+        menuOpen.classList.add('show');
+    } else {
+        menuOpen.classList.remove('show');
+    }
+});
 
 // Left Menu DropDowns
 
-let leftMenuDrops = document.querySelectorAll('#the-menus>ul>li.drop>a')
-let leftMenuDropDowns = document.querySelectorAll('#the-menus>ul>li.drop>ul')
+leftMenuDrops.forEach(dropElement => {
+    dropElement.addEventListener('click', (e) => {
+        let dropItem = dropElement.querySelector(".drop");
+        let dropItemIcon = dropElement.querySelector(".fas");
+        if (!dropItem.classList.contains("show")) {
+            dropItem.classList.add('show');
+            dropItemIcon.classList.replace("fa-caret-down", "fa-caret-up");
+
+        } else {
+            dropItem.classList.remove('show');
+            dropItemIcon.classList.replace("fa-caret-up", "fa-caret-down");
+        }
+    });
+});
 
 
-leftMenuDrops.forEach(drop=>{
-    drop.addEventListener('click',(e)=>{
-        e.preventDefault()
+//fullscreen
 
-        leftMenuDropDowns.forEach(dropDown=>{
-            if(dropDown!=drop.nextElementSibling){
+function toggleFullScreen() {
+    if (window.innerHeight == screen.height) {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) {
+            document.msExitFullscreen();
+        }
+    }
+    if (!document.mozFullScreen && !document.webkitFullScreen) {
+        if (document.body.mozRequestFullScreen) {
+            document.body.mozRequestFullScreen();
+        } else {
+            document.body.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    }
+}
 
-                dropDown.classList.remove('active')
-            }
-            
-        })
+document.querySelector("#fullscreen").addEventListener("click", e => {
+    if (window.innerHeight == screen.height) {
+        e.target.src = "Images/fs_open.svg";
+    } else {
+        e.target.src = "Images/fs_close.svg";
+    }
+    toggleFullScreen();
+}, false);
 
-        drop.nextElementSibling.classList.toggle('active')
+// language
 
-       
+document.querySelector("#language").addEventListener("click", e => {
+    document.querySelector("#language_list").classList.toggle("show");
+    document.querySelector("#language_list").style.top = `${e.target.offsetTop + e.target.width}px`;
+    document.querySelector("#language_list").style.left = `${e.target.offsetLeft - e.target.height}px`;
+}, false);
 
-     
-    })
-})
+document.querySelectorAll(".language_item").forEach(item => {
+    item.addEventListener("click", e => {
+        if (!(e.target.tagName == "LI")) {
+            let language_src = e.target.parentElement.childNodes[0].src;
+            let language_name = e.target.parentElement.childNodes[0].alt;
+            e.target.parentElement.childNodes[0].src = document.querySelector("#language").src;
+            e.target.parentElement.childNodes[0].alt = document.querySelector("#language").alt;
+            document.querySelector("#language").src = language_src;
+            e.target.parentElement.childNodes[2].textContent = document.querySelector("#language").alt;
+            document.querySelector("#language").alt = language_name;
+        }
+    });
+});
 
+//user
 
-//Close Left-Menu 
+document.querySelector("#user_demo").addEventListener("click", e => {
+    document.querySelector("#user_demo_list").classList.toggle("show");
+    document.querySelector("#user_demo_list").style.top = `${document.querySelector("#user_demo").offsetTop + document.querySelector("#user_demo").offsetWidth / 2}px`;
+    document.querySelector("#user_demo_list").style.left = `${document.querySelector("#user_demo").offsetLeft - document.querySelector("#user_demo").offsetHeight / 2}px`;
+}, false);
 
-let closeButton = document.getElementById('close-button')
-let companyName = document.getElementById('company')
-let leftMenuColumn= document.getElementById('the-menus')
-
-closeButton.addEventListener('click',(e)=>{
-    companyName.classList.toggle('closed')
-    leftMenuColumn.classList.toggle('closed')
-
-})
+window.onclick = function (event) {
+    if (!event.target.matches("#language")) {
+        document.querySelector("#language_list").classList.remove("show");
+    }
+    if (!(event.target.matches("#user_demo") || event.target.matches("#user_name") || event.target.matches("#user_image"))) {
+        document.querySelector("#user_demo_list").classList.remove("show");
+    }
+}
