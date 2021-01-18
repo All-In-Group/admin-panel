f5b4e4bbb9e24b5c6bcf050bebdb978ac8a139b8
Vardan135 <67186965+Vardan135@users.noreply.github.com>
2021-01-18 11:32:25 +0400

    Update grafic.js

--- Final Version/lib/grafic.js
+++ Final Version/lib/grafic.js
@@ -1,9 +1,9 @@
 let grafic_1_data = {
     chart: {
-        type: 'line'
+        type: "line"
     },
     series: [{
-        name: 'sales',
+        name: "sales",
         data: [10, 25, 15, 30]
     }],
     xaxis: {
@@ -15,14 +15,14 @@ let grafic_2_data = {
         data: [17, 25, 60, 78, 95]
     }],
     chart: {
-        type: 'area',
+        type: "area",
         height: 160,
         sparkline: {
             enabled: true
         },
     },
     stroke: {
-        curve: 'straight'
+        curve: "straight"
     },
     fill: {
         opacity: 0.3,
@@ -30,35 +30,35 @@ let grafic_2_data = {
     yaxis: {
         min: 0
     },
-    colors: ['#DCE6EC'],
+    colors: ["#DCE6EC"],
     title: {
-        text: '$462,020',
+        text: "$462,020",
         offsetX: 0,
         style: {
-            fontSize: '24px',
+            fontSize: "24px",
         }
     },
     subtitle: {
-        text: 'Sales',
+        text: "Sales",
         offsetX: 0,
         style: {
-            fontSize: '14px',
+            fontSize: "14px",
         }
     }
 };
-let grafic_3_data = {
+var grafic_3_data = {
     series: [{
         data: [4, 12, 30, 58, 79]
     }],
     chart: {
-        type: 'area',
+        type: "area",
         height: 160,
         sparkline: {
             enabled: true
         },
     },
     stroke: {
-        curve: 'straight'
+        curve: "straight"
     },
     fill: {
         opacity: 0.3,
@@ -66,19 +66,19 @@ let grafic_3_data = {
     yaxis: {
         min: 0
     },
-    colors: ['#DCE6EC'],
+    colors: ["#DCE6EC"],
     title: {
-        text: '$169,322',
+        text: "$169,322",
         offsetX: 0,
         style: {
-            fontSize: '24px',
+            fontSize: "24px",
         }
     },
     subtitle: {
-        text: 'Expenses',
+        text: "Expenses",
         offsetX: 0,
         style: {
-            fontSize: '14px',
+            fontSize: "14px",
         }
     }
 };
@@ -87,14 +87,14 @@ let grafic_4_data = {
         data: [44, 55, 41, 17, 15]
     }],
     chart: {
-        type: 'area',
+        type: "area",
         height: 160,
         sparkline: {
             enabled: true
         },
     },
     stroke: {
-        curve: 'straight'
+        curve: "straight"
     },
     fill: {
         opacity: 0.3
@@ -108,29 +108,29 @@ let grafic_4_data = {
         min: 0
     },
     title: {
-        text: '$295,965',
+        text: "$295,965",
         offsetX: 0,
         style: {
-            fontSize: '24px',
+            fontSize: "24px",
         }
     },
     subtitle: {
-        text: 'Profits',
+        text: "Profits",
         offsetX: 0,
         style: {
-            fontSize: '14px',
+            fontSize: "14px",
         }
     }
 };
 
 let grafic_5_data = {
     series: [{
-        name: 'Series 1',
-        data: [20, 100, 40, 30, 50, 80, 33],
+        name: "Series 1",
+        data: [20, 90, 40, 30, 50, 80, 33],
     }],
     chart: {
         height: 350,
-        type: 'radar',
+        type: "radar",
     },
     dataLabels: {
         enabled: true
@@ -139,21 +139,19 @@ let grafic_5_data = {
         radar: {
             size: 140,
             polygons: {
-                strokeColors: '#e9e9e9',
+                strokeColors: "#e9e9e9",
                 fill: {
-                    colors: ['#f8f8f8', '#fff']
+                    colors: ["#f8f8f8", "#fff"]
                 }
             }
         }
     },
-    title: {
-        text: 'Radar with Polygon Fill'
-    },
-    colors: ['#FF4560'],
+    //title: { text: "Radar" },
+    colors: ["#FF4560"],
     markers: {
         size: 4,
-        colors: ['#fff'],
-        strokeColor: '#FF4560',
+        colors: ["#fff"],
+        strokeColor: "#FF4560",
         strokeWidth: 2,
     },
     tooltip: {
@@ -164,7 +162,7 @@ let grafic_5_data = {
         }
     },
     xaxis: {
-        categories: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        categories: ["A", "B", "C", "D", "E", "F", "G"]
     },
     yaxis: {
         tickAmount: 7,
@@ -173,7 +171,7 @@ let grafic_5_data = {
                 if (i % 2 === 0) {
                     return val
                 } else {
-                    return ''
+                    return ""
                 }
             }
         }
@@ -182,11 +180,11 @@ let grafic_5_data = {
 
 let grafic_6_data = {
     chart: {
-        type: 'pie'
+        type: "pie"
     },
     series: [44, 55, 41, 17, 15],
     chartOptions: {
-        labels: ['Apple', 'Mango', 'Orange', 'Watermelon']
+        labels: ["Apple", "Mango", "Orange", "Watermelon"]
     }
 };
 
